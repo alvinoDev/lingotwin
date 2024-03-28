590e345108e48d80ba65c5166c4b71752f448406
alvinoDev <48383924+alvinoDev@users.noreply.github.com>
2024-03-28 09:21:06 -0400

    Feature: Setup, Buttons, MArketing page, Autentication, Main Layout, Sidebar

---
+++ app/(main)/learn/page.tsx
@@ -0,0 +1,17 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+
+const LearnPage = () => {
+	return (
+		<div className="flex flex-row-reverse gap-[48px] px-6">
+			<StickyWrapper>
+				My sticky sidebar
+			</StickyWrapper>
+			<FeedWrapper>
+				My feed, min 07:11
+			</FeedWrapper>
+		</div>
+	);
+};
+
+export default LearnPage;
---
+++ app/(marketing)/footer.tsx
@@ -0,0 +1,61 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+export const Footer = () => {
+	return (
+		<footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+			<div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+				<Button size="lg" variant="ghost" className="w-full">
+					<Image
+						src="/es.svg"
+						alt="Spanish"
+						width={40}
+						height={32}
+						className="mr-4 rounded-md"
+					/>
+					Spanish
+				</Button>
+				<Button size="lg" variant="ghost" className="w-full">
+					<Image
+						src="/fr.svg"
+						alt="French"
+						width={40}
+						height={32}
+						className="mr-4 rounded-md"
+					/>
+					French
+				</Button>
+				<Button size="lg" variant="ghost" className="w-full">
+					<Image
+						src="/it.svg"
+						alt="Italian"
+						width={40}
+						height={32}
+						className="mr-4 rounded-md"
+					/>
+					Italian
+				</Button>
+				<Button size="lg" variant="ghost" className="w-full">
+					<Image
+						src="/jp.svg"
+						alt="Japanese"
+						width={40}
+						height={32}
+						className="mr-4 rounded-md"
+					/>
+					Japanese
+				</Button>
+				<Button size="lg" variant="ghost" className="w-full">
+					<Image
+						src="/hr.svg"
+						alt="Croatian"
+						width={40}
+						height={32}
+						className="mr-4 rounded-md"
+					/>
+					Croatian
+				</Button>
+			</div>
+		</footer>
+	);
+};
